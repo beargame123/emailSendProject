fcb8e06a250144fa777c98edc77f8d08312a960b
beargame123 <hoyoung7827@gmail.com>
2022-05-19 15:43:07 +0900

    ➕MailDto 구현

---
+++ emailSendProject/src/main/java/com/example/emailsendproject/dto/MailDto.java
@@ -0,0 +1,12 @@
+package com.example.emailsendproject.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MailDto {
+    private String address;
+    private String title;
+    private String message;
+}
