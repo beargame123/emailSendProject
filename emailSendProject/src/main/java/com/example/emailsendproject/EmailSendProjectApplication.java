3391f5c0087afd4e3ceee8d77c74e4ce09c36531
beargame123 <hoyoung7827@gmail.com>
2022-05-19 15:42:44 +0900

    💻초기 세팅

---
+++ emailSendProject/src/main/java/com/example/emailsendproject/EmailSendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailsendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSendProjectApplication.class, args);
+    }
+
+}
