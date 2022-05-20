bdfd3e9953f14c8ca487b45bcb0d169210d81aec
beargame123 <hoyoung7827@gmail.com>
2022-05-20 13:33:19 +0900

    🛠️MailController 수정

--- emailSendProject/src/main/java/com/example/emailsendproject/controller/MailController.java
+++ emailSendProject/src/main/java/com/example/emailsendproject/controller/MailController.java
@@ -3,7 +3,6 @@ package com.example.emailsendproject.controller;
 import com.example.emailsendproject.dto.MailDto;
 import com.example.emailsendproject.service.MailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +14,7 @@ public class MailController {
     private final MailService mailService;
 
     @PostMapping("/mail")
-    public String execMail(@RequestBody MailDto mailDto){
+    public String execMail(MailDto mailDto){
         mailService.mailSend(mailDto);
         return "메일 정상적으로 보내줬습니다.";
     }
