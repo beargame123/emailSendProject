3b242f7f0dc43be162e512f2bad2c6e703f8e69a
beargame123 <hoyoung7827@gmail.com>
2022-05-20 13:32:46 +0900

    🛠️MailDto 수정

--- emailSendProject/src/main/java/com/example/emailsendproject/dto/MailDto.java
+++ emailSendProject/src/main/java/com/example/emailsendproject/dto/MailDto.java
@@ -2,6 +2,7 @@ package com.example.emailsendproject.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -9,4 +10,5 @@ public class MailDto {
     private String address;
     private String title;
     private String message;
+    private MultipartFile file;
 }
